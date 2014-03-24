2ca7657fb6dcf04e70b69b25786e493211ccd2f8
Akif Khan <akif500@gmail.com>
2014-03-24 22:39:26 +0530

    Readme and info added

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+//Note:  Do not include the opening and closing PHP tags when you copy this code
+?>
